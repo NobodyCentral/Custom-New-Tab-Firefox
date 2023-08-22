6d271a61d9a18d5aac91621b04237edc535e2c87
Nobody <Nobody@NOBODYALPHABASE>
2023-08-21 23:15:41 -0700

    #

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-window.location.href = localStorage.getItem('url');
+const url = localStorage.getItem('url');
+
+if (url) {
+    window.location.href = url;
+} else {
+    window.location.href = 'https://www.google.com';
+};
